f03b010beb4a8d806f0b456f56b0ccd87b714abb
TojoMasahiko <jotonne55@gmail.com>
2025-02-25 23:31:25 +0900

    feat: imple consumer

--- consumer/main.go
+++ consumer/main.go
@@ -1,7 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"kafka-practice/producer/dto"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+var (
+	kafkaServer  string
+	kafkaTopic   string
+	kafkaGroupID string
+)
 
 func main() {
-	fmt.Println("Hello Cunsumer")
+	parse()
+
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": kafkaServer,
+		"group.id":          kafkaGroupID,
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer consumer.Close()
+
+	if err := consumer.SubscribeTopics([]string{kafkaTopic}, nil); err != nil {
+		panic(err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				msg, err := consumer.ReadMessage(1 * time.Second)
+				if err != nil {
+					if err.(kafka.Error).Code() == kafka.ErrTimedOut {
+						fmt.Println("time out")
+						continue
+					}
+					fmt.Printf("%s\n", err.Error())
+				}
+				var consumedMessage dto.Message
+				err = json.Unmarshal(msg.Value, &consumedMessage)
+				if err != nil {
+					fmt.Printf("%s\n", err.Error())
+				}
+				fmt.Printf("%+v\n", consumedMessage)
+			}
+		}
+	}()
+
+	<-sigchan
+}
+
+func parse() {
+	flag.StringVar(&kafkaServer, "kafkaServer", "localhost:9092", "kafka server")
+	flag.StringVar(&kafkaTopic, "kafkaTopic", "topic1", "topic name")
+	flag.StringVar(&kafkaGroupID, "kafkaGroupID", "group1", "group id")
+	flag.Parse()
 }
